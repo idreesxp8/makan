dc6747155819ac617bd27161ee6381b4c3ce8413
flopmart <34133163+asadnisar98@users.noreply.github.com>
2022-04-11 12:48:46 +0500

    slider inside slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,48 @@
 $(document).ready(function () {
 
+  
+  $('.properties_inner_crousal').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true
+  });
+  $('.properties_crousal').slick({
+    autoplay:false,
+    autoplaySpeed:2000,
+    dots: false,
+    pauseOnHover: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+
+
 $('.hero_slider').slick({
     autoplay:false,
     autoplaySpeed:2000,
@@ -35,6 +78,6 @@ $('.hero_slider').slick({
         },
       },
     ],
-  })
+  });
 
-})
+});
