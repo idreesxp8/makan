357a27672bcc1d815f05f38af19bf52ecc2ab93d
Ahsan Zahoor <34133163+asadnisar98@users.noreply.github.com>
2022-04-14 12:03:06 +0500

    new

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-<<<<<<< Updated upstream
 
 
 
@@ -11,28 +10,6 @@ $(document).ready(function () {
     placeholder: "Select a programming language",
     allowClear: true,
   });
-=======
-  // Select2
-  // $("#single").select2({
-  //   placeholder: "Select a programming language",
-  //   allowClear: true,
-  // });
-  $("#multiple").select2({
-    placeholder: "Select a Location",
-    allowClear: true,
-  });
-
-  $("#multiple2").select2({
-    placeholder: "Select a Sale",
-    allowClear: true,
-  });
-
-  $("#multiple3").select2({
-    placeholder: "Select a Appartment",
-    allowClear: true,
-  });
-
->>>>>>> Stashed changes
   // Mega Menu
 
   $(".accordion_menu_single a").click(function () {
